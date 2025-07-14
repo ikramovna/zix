28eb37212f84969191edcc9ec28ad63d8076b79c
muslima <muslimazokirjonova2004@gmail.com>
2025-07-14 23:12:19 +0500

    check ci/cd

--- root/swagger.py
+++ root/swagger.py
@@ -5,7 +5,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="ZIXX API",
+        title="ZIX API",
         default_version='v1',
         description="**Created by Ikramovna**\n\n**ZIXX API Documentation**\n\n**Version 1.0**",
         terms_of_service="https://www.google.com/policies/terms/",
